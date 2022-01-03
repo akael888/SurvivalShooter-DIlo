f5a3ddac53cc279070d6f9342c14fa60111e65e5
Elgratio Latuihamallo <akael888@gmail.com>
2022-01-04 06:06:19 +0700

    Done

---
+++ Assets/Scripts/Interface/Concept/ShapeFactory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ShapeFactory
+{
+
+    //use getShape method to get object of type shape 
+    public Shape getShape(string shapeType)
+    {
+        if (shapeType == null)
+        {
+            return null;
+        }
+        if (String.Compare(shapeType, "CIRCLE", true) == 0)
+        {
+            return new Circle();
+
+        }
+        else if (String.Compare(shapeType, "RECTANGLE", true) == 0)
+        {
+            return new Rectangle();
+
+        }
+        else if (String.Compare(shapeType, "SQUARE", true) == 0)
+        {
+            return new Square();
+        }
+
+        return null;
+    }
+}
---
+++ Assets/Scripts/Interface/EnemyFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using UnityEngine;
+
+public class EnemyFactory : MonoBehaviour, IFactory
+{
+
+    [SerializeField]
+    public GameObject[] enemyPrefab;
+
+    public GameObject FactoryMethod(int tag)
+    {
+        GameObject enemy = Instantiate(enemyPrefab[tag]);
+        return enemy;
+    }
+}
